2fddb304e3bd0355e42052ce62a621fa1351fb08
Lofz <52474509+Lofz@users.noreply.github.com>
2020-03-03 22:29:17 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,7 +9,6 @@ function setup() {
   getTopics();
   createSectionLinks();
   createCheckList();
-  // getParagraph();
 }
 
 function getTopics() {
@@ -18,25 +17,38 @@ function getTopics() {
     var firstChar = txt[i].charAt(0);
     txt[i] = txt[i].replace(firstChar, '');
 
+    //make h3 title
     if (secondChar == '*') {
       var topicsList = createTopicsList(firstChar);
       txt[i] = txt[i].replace('* ', '');
       var sectionTopics = document.createElement("h3");
+      sectionTopics.innerHTML = txt[i];
+      txt[i] = txt[i].replace(/:/g, '');
       sectionTopics.id = txt[i];
       sectionTopics.setAttribute("name", txt[i]);
-      sectionTopics.innerHTML = txt[i];
       topicsList.appendChild(sectionTopics);
     }
 
+    //make paragraph
     if (secondChar == '-') {
       txt[i] = txt[i].replace(/- /g, '');
       var newParagraph = document.createElement("p");
       newParagraph.innerHTML = txt[i];
       topicsList.appendChild(newParagraph);
     }
+
+    //make image
+    if (secondChar == '#') {
+      txt[i] = txt[i].replace(/# /g, '');
+      var newImage = document.createElement("img");
+      newImage.setAttribute("class", "exampleImg");
+      newImage.src = txt[i]
+      topicsList.appendChild(newImage);
+    }
   }
 }
 
+//make whole div
 function createTopicsList(firstChar) {
   var getSection = document.getElementById("testsSection-" + firstChar);
   console.log(getSection);
